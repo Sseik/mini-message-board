2d71b707078c4ced1fec7ac14a616d1ae2c992d8
Dmytro Hapchyn <gapcindmitro@gmail.com>
2026-06-27 19:16:44 +0300

    Initial commit

---
+++ app.js
@@ -0,0 +1,21 @@
+import express from "express";
+import indexRouter from "./routes/indexRouter.js";
+import path from "node:path";
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+app.set("view engine", "ejs");
+
+const assetsPath = path.join(import.meta.dirname, "public");
+app.use(express.static(assetsPath));
+
+app.use("/", indexRouter);
+
+app.listen(PORT, (err) => {
+  if (err) {
+    console.error(err);
+    throw err;
+  }
+  console.log("Started server");
+});
---
+++ routes/indexRouter.js
@@ -0,0 +1,20 @@
+import { Router } from "express";
+
+const messages = [
+  {
+    text: "Hi there!",
+    user: "Amando",
+    added: new Date()
+  },
+  {
+    text: "Hello World!",
+    user: "Charles",
+    added: new Date()
+  }
+];
+
+const indexRouter = Router();
+
+indexRouter.get("/", (req, res) => res.render("index", { messages }));
+
+export default indexRouter;
